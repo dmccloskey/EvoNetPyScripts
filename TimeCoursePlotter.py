a1bcbdd7a2e27bbfcc328c18efc3ce50b737184b
dmccloskey <dmccloskey87@gmail.com>
2020-05-05 23:01:45 +0200

    WIP: updating new script

--- TimeCoursePlotter.py
+++ TimeCoursePlotter.py
@@ -2,33 +2,39 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 
-def plotTimeCourse(index_name, headers, data, axs, color, marker, label):
+def plotTimeCourse(n_node, memory_size, subplot_titles, input_data, output_data, expected_data, nodes_to_labels, axs, color, marker):
     """Generate a side by side plot of the input node values, output node values, and expected output node values.
     Each figure is of a single train/text batch.
     Each panel is of a single node.
 
     Args:
-        headers: list of train/test data type names
-        data:
+        n_node
+        subplot_titles
+        input_data
+        output_data
+        expected_data
+        nodes_to_labels
         axs
         color: the color to use for the plots
         maker: the marker to use for the plots
-        label: the label to use for the plots
     """
-    n_cols = len(headers)
-    n_rows = 20; # FIXME
-    data_downsampled = data.iloc[::2] # down sample by 1/2
-    for n_col in range(0,n_cols):
-        for n_row in range(0,n_rows):
-            if n_row==0:
-                axs[n_row, n_col].scatter(data_downsampled.loc[:, index_name],  data_downsampled.loc[:, headers[n_col]],
-                           alpha=0.5, c=color, marker=marker, edgecolors='none', s=20, label=label)
-                #axs[n_row, n_row].title.set_text("Train")
-            else:
-                axs[n_row, n_col].scatter(data_downsampled.loc[:, index_name],  data_downsampled.loc[:, headers[n_col]],
-                           alpha=0.5, c=color, marker=marker, edgecolors='none', s=20)
+    n_cols = len(subplot_titles)
+    x_data = np.arrange(memory_size)
+    label = nodes_to_labels[nodes_to_labels["nodes"]==n_node]["labels"].to_series()[0]
 
-def makeColumnHeaders(n_batch, n_node, memory_size): 
+    # Make the subplots
+    axs[n_node, 0].scatter(x_data, input_data.iloc[0],
+                alpha=0.5, c=color, marker=marker, edgecolors='none', s=20, label=label)
+    axs[n_node, 1].scatter(x_data, output_data.iloc[0],
+                alpha=0.5, c=color, marker=marker, edgecolors='none', s=20, label=label)
+    axs[n_node, 2].scatter(x_data, expected_data.iloc[0],
+                alpha=0.5, c=color, marker=marker, edgecolors='none', s=20, label=label)
+    if n_node==0:
+        axs[n_node, 0].title.set_text(subplot_titles[0])
+        axs[n_node, 1].title.set_text(subplot_titles[1])
+        axs[n_node, 2].title.set_text(subplot_titles[2])
+
+def makeColumnHeaders(n_batch, n_node, memory_size, index_name): 
     """Make the expected headers for the input, output, and expected data files.
 
     Args:
@@ -40,82 +46,13 @@ def makeColumnHeaders(n_batch, n_node, memory_size):
     Returns:
         pandas data frame
     """   
-    input_headers = ["Input_{:012d}_Input_Batch-{}_Memory-{}".format(n_node, n_batch, j) for j in range(memory_size)]
-    output_headers = ["Output_{:012d}_Output_Batch-{}_Memory-{}".format(n_node, n_batch, j) for j in range(memory_size)]
-    expected_headers = ["Output_{:012d}_Expected_Batch-{}_Memory-{}".format(n_node, n_batch, j) for j in range(memory_size)]
-
-def aggregateTrainTestStats(headers, agg_funcs, index_name, data, label, n_agg_values = 10):
-    """Creates a pandas data frame with headers according to headers
-    and row labels according to labels where the column data from the
-    specified header is aggregated according to the specified aggregation function.
-    In addition, the number of iterations required to reach the aggregation function
-    100 times is also reported.
-
-    Args:
-        headers: list of train/test metric names
-        agg_func: name of an aggregation function
-        index_name: name of the index header
-        data: the actual data frame of values to aggregate according to the specified statistics
-
-    Returns:
-        pandas data frame
-    """
-    assert(len(headers)==len(agg_funcs))
-    row_data = {"label":label}    
-    for header_tup, agg_func in zip(headers, agg_funcs):
-        for header in header_tup:
-            # calculate the aggregation statistic
-            agg_func_value = None
-            if agg_func == "max":
-                agg_func_value = data.loc[:,header].abs().max() # just use the magnitude and not the direction
-            elif agg_func == "min":
-                agg_func_value = data.loc[:,header].abs().min() # just use the magnitude and not the direction
-            row_data.update({header:agg_func_value})
-
-            # calculate # of iterations using the agg stat
-            indices = []
-            if agg_func == "max":
-                indices = data[data[header].abs()>=agg_func_value*0.98].index.tolist()
-            elif agg_func == "min":
-                indices = data[data[header].abs()<=agg_func_value*1.02].index.tolist()
-            # TODO: added only for loss error
-            n_agg_values = 10
-            if "_Error" in header:
-                n_agg_values = 1
-            index = indices[n_agg_values-1] if len(indices) >= n_agg_values else np.NaN
-            row_data.update({header + "_itersToValue":index})
-    return row_data
-
-def readDataFilenamesCsv(filename):
-    """Creates a pandas data frame with headers for 'filenames','labels','color','marker'
-    from a .csv file and returns each of the columns as seperate entities.  The column 'used_' is
-    used to select what rows to use
-
-    Args:
-        filename: name of the file
-
-    Returns:
-        pandas data frame with headers for 'filenames','labels','colors','markers'
-    """
-    data = pd.read_csv(filename)
-    data_used = data[data["used_"]==True]
-    return data_used
-
-def readDataNodes(filename):
-    """Creates a pandas data frame with headers for 'nodes','labels'
-    from a .csv file and returns each of the columns as seperate entities.  The column 'used_' is
-    used to select what rows to use
-
-    Args:
-        filename: name of the file
-
-    Returns:
-        pandas data frame with headers for 'nodes','labels'
-    """
-    data = pd.read_csv(filename)
-    data_used = data[data["used_"]==True]
-
-    return data_used
+    input_headers = ["Input_{:012d}_Input_Batch-{}_Memory-{}".format(n_node, n_batch, j) for j in range(0, memory_size)]
+    output_headers = ["Output_{:012d}_Output_Batch-{}_Memory-{}".format(n_node, n_batch, j) for j in range(0, memory_size)]
+    expected_headers = ["Output_{:012d}_Expected_Batch-{}_Memory-{}".format(n_node, n_batch, j) for j in range(0, memory_size)]
+    input_headers.append(index_name)
+    output_headers.append(index_name)
+    expected_headers.append(index_name)
+    return input_headers, output_headers, expected_headers
 
 def main(data_dir, data_filename, nodes_filename, index_name, n_epoch, n_batch, n_nodes, memory_size):
     """Run main script"""
@@ -127,35 +64,37 @@ def main(data_dir, data_filename, nodes_filename, index_name, n_epoch, n_batch, 
     nodes_to_labels = nodes_to_labels[nodes_to_labels["used_"]==True] # filter on used
 
     # make the initial figure
-    if display_plot:
-        fig, axs = plt.subplots(n_nodes, 3, sharex=True, sharey=False)
+    fig, axs = plt.subplots(n_nodes, 3, sharex=True, sharey=False)
 
     # read in the data and anlayze each train/test metric
     all_colors = ["b","r","g","m","c","y","k"]
     all_markers = [".","+",",","o","x","v"]
+    subplot_titles = ["Input", "Output", "Expected"]
     marker_iter = 0
     color_iter = 0
     for n in range(0, n_nodes):
         # make the expected column headers
-        input_headers, output_headers, expected_headers = makeColumnHeaders(n_batch, n, memory_size) 
+        input_headers, output_headers, expected_headers = makeColumnHeaders(index_name, n_batch, n, memory_size) 
 
-        # read in and trim the data
-        input_data = pd.read_csv(filenames['input_filenames'], usecols = input_headers, dtype=np.float32)
-        input_data = input_data[input_data[index_name]==n_epoch] # filter on epoch
-        output_data = pd.read_csv(filenames['output_filenames'], usecols = output_headers, dtype=np.float32)
-        output_data = output_data[output_data[index_name]==n_epoch] # filter on epoch
-        expected_data = pd.read_csv(filenames['expected_filenames'], usecols = expected_headers, dtype=np.float32)
-        expected_data = expected_data[expected_data[index_name]==n_epoch] # filter on epoch
+        for series in range(0,len(filenames)):
+            # read in and trim the data
+            input_data = pd.read_csv(filenames['input_filenames'][series], usecols = input_headers, dtype=np.float32)
+            print(len(input_data))
+            input_data = input_data[input_data[index_name]==n_epoch] # filter on epoch
+            output_data = pd.read_csv(filenames["output_filenames"][series], usecols = output_headers, dtype=np.float32)
+            output_data = output_data[output_data[index_name]==n_epoch] # filter on epoch
+            expected_data = pd.read_csv(filenames["expected_filenames"][series], usecols = expected_headers, dtype=np.float32)
+            expected_data = expected_data[expected_data[index_name]==n_epoch] # filter on epoch
 
-        # plot each node time-course
-        plotTrainTest(index_name, headers, data, 
-                        axs, all_colors[color_iter], all_markers[marker_iter], labels[n])
-        color_iter += 1
-        if color_iter >= len(all_colors):
-            color_iter = 0;
-            marker_iter += 1
-            if marker_iter >= len(all_markers):
-                marker_iter = 0
+            # plot each node time-course
+            plotTimeCourse(n, memory_size, subplot_titles, input_data, output_data, expected_data, nodes_to_labels,
+                            axs, all_colors[color_iter], all_markers[marker_iter])
+            color_iter += 1
+            if color_iter >= len(all_colors):
+                color_iter = 0;
+                marker_iter += 1
+                if marker_iter >= len(all_markers):
+                    marker_iter = 0
         
     # make the legend
     axs[0,0].legend(loc="upper left", markerscale=2)
