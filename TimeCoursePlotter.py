fe63b8ec6434826b86bdf53da235f38d795e225d
dmccloskey <dmccloskey87@gmail.com>
2020-05-07 21:22:59 +0200

    fix: working plots.  colors can be fixed

--- TimeCoursePlotter.py
+++ TimeCoursePlotter.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 
-def plotTimeCourse(n_node, memory_size, subplot_titles, input_data, output_data, expected_data, nodes_to_labels, axs, color, marker):
+def plotTimeCourse(n_node, n_row, memory_size, subplot_titles, input_data, output_data, expected_data, nodes_to_labels, axs, color, marker, series):
     """Generate a side by side plot of the input node values, output node values, and expected output node values.
     Each figure is of a single train/text batch.
     Each panel is of a single node.
@@ -19,19 +19,22 @@ def plotTimeCourse(n_node, memory_size, subplot_titles, input_data, output_data,
         maker: the marker to use for the plots
     """
     x_data = np.arange(memory_size)
-    label = nodes_to_labels[nodes_to_labels["nodes"]==n_node]["labels"]
+    label = nodes_to_labels[nodes_to_labels["nodes"]==n_node]["labels"][n_node]
 
     # Make the subplots
-    axs[n_node, 0].scatter(x_data, input_data.iloc[0][1:],
-                alpha=0.5, c=color, marker=marker, edgecolors='none', s=20, label=label)
-    axs[n_node, 1].scatter(x_data, output_data.iloc[0][1:],
-                alpha=0.5, c=color, marker=marker, edgecolors='none', s=20, label=label)
-    axs[n_node, 2].scatter(x_data, expected_data.iloc[0][1:],
-                alpha=0.5, c=color, marker=marker, edgecolors='none', s=20, label=label)
-    if n_node==0:
-        axs[n_node, 0].title.set_text(subplot_titles[0])
-        axs[n_node, 1].title.set_text(subplot_titles[1])
-        axs[n_node, 2].title.set_text(subplot_titles[2])
+    axs[n_row, 0].scatter(x_data, input_data.iloc[0][1:],
+                alpha=0.5, c=color, marker=marker, edgecolors='none', s=20, label=series)
+    axs[n_row, 1].scatter(x_data, output_data.iloc[0][1:],
+                alpha=0.5, c=color, marker=marker, edgecolors='none', s=20, label=series)
+    axs[n_row, 2].scatter(x_data, expected_data.iloc[0][1:],
+                alpha=0.5, c=color, marker=marker, edgecolors='none', s=20, label=series)
+    # Make the titles 
+    axs[n_row, 0].title.set_text("{} {}".format(label, subplot_titles[0]))
+    axs[n_row, 1].title.set_text("{} {}".format(label, subplot_titles[1]))
+    axs[n_row, 2].title.set_text("{} {}".format(label, subplot_titles[2]))
+
+    # make the legend
+    axs[n_row,0].legend(loc="upper left", markerscale=2)
 
 def makeColumnHeaders(n_batch, n_node, memory_size, index_name): 
     """Make the expected headers for the input, output, and expected data files.
@@ -53,7 +56,7 @@ def makeColumnHeaders(n_batch, n_node, memory_size, index_name):
     expected_headers.append(index_name)
     return input_headers, output_headers, expected_headers
 
-def main(data_dir, data_filename, nodes_filename, index_name, n_epoch, n_batch, n_nodes, memory_size):
+def main(data_dir, data_filename, nodes_filename, index_name, n_epoch, n_batch, nodes, memory_size):
     """Run main script"""
 
     # read in the input files
@@ -63,7 +66,9 @@ def main(data_dir, data_filename, nodes_filename, index_name, n_epoch, n_batch, 
     nodes_to_labels = nodes_to_labels[nodes_to_labels["used_"]==True] # filter on used
 
     # make the initial figure
+    n_nodes = len(nodes)
     fig, axs = plt.subplots(n_nodes, 3, sharex=True, sharey=False)
+    print("Preparing the plot...")
 
     # read in the data and anlayze each train/test metric
     all_colors = ["b","r","g","m","c","y","k"]
@@ -71,7 +76,8 @@ def main(data_dir, data_filename, nodes_filename, index_name, n_epoch, n_batch, 
     subplot_titles = ["Input", "Output", "Expected"]
     marker_iter = 0
     color_iter = 0
-    for n in range(0, n_nodes):
+    for n_row, n in enumerate(nodes):
+        print("adding node {}...".format(n))
         # make the expected column headers
         input_headers, output_headers, expected_headers = makeColumnHeaders(n_batch, n, memory_size, index_name)
 
@@ -85,32 +91,31 @@ def main(data_dir, data_filename, nodes_filename, index_name, n_epoch, n_batch, 
             expected_data = expected_data[expected_data[index_name]==n_epoch] # filter on epoch
 
             # plot each node time-course
-            plotTimeCourse(n, memory_size, subplot_titles, input_data, output_data, expected_data, nodes_to_labels,
-                            axs, all_colors[color_iter], all_markers[marker_iter])
+            plotTimeCourse(n, n_row, memory_size, subplot_titles, input_data, output_data, expected_data, nodes_to_labels,
+                            axs, all_colors[color_iter], all_markers[marker_iter], filenames['series'][series])
             color_iter += 1
             if color_iter >= len(all_colors):
                 color_iter = 0;
                 marker_iter += 1
                 if marker_iter >= len(all_markers):
                     marker_iter = 0
-        
-    # make the legend
-    axs[0,0].legend(loc="upper left", markerscale=2)
 
     # show the image
+    print("Showing the plot...")
     plt.show()
 
 # Run main
 if __name__ == "__main__":
     # Input files
-    data_dir = "C:/Users/dmccloskey/Documents/GitHub/EvoNetData/MNIST_examples/KineticModel/Gpu0-0a/"
+    data_dir = ""
     data_filename = data_dir + "TimeCourseFilenames.csv"
     nodes_filename = data_dir + "TimeCourseNodes.csv"
 
     # Input parameters
     index_name = "Epoch"
-    n_epoch = 0
+    n_epoch = 9000
     n_batch = 0
-    n_nodes = 23
+    #nodes = [0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
+    nodes = [11, 19, 20, 22]
     memory_size = 128
-    main(data_dir, data_filename, nodes_filename, index_name, n_epoch, n_batch, n_nodes, memory_size)
+    main(data_dir, data_filename, nodes_filename, index_name, n_epoch, n_batch, nodes, memory_size)
