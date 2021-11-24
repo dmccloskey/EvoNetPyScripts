9f3dd82f5cb6c53898155996e534b83848071b06
dmccloskey <dmccloskey87@gmail.com>
2021-11-24 23:31:55 +0100

    feat, example: added new plotting method for barplot with errors and multi color indices along with dummy example data

---
+++ MultiIndicesBarPlot.py
@@ -0,0 +1,144 @@
+import pandas as pd
+import numpy as np
+import io
+import matplotlib.pyplot as plt
+import matplotlib.patches as pch
+from matplotlib.lines import Line2D
+from itertools import cycle, chain
+
+def dicrete_plot(df, index_columns, metric_columns, error_columns, unit=1.0, scale=1.0, border=2.0, legend_offset=1.0, capsize=10):
+    # Initial assertions:
+    if len(index_columns) == 0:
+        raise ValueError('Empty list given, index_columns must be non-empty list')
+    if len(metric_columns) == 0 or len(error_columns) == 0:
+        raise ValueError('Empty list given, metric_columns and error_columns must be non-empty list')
+    if len(metric_columns) != len(error_columns):
+        raise ValueError('metric_columns and error_columns must be lists of equal size')
+
+    # Extract labels:
+    labels = {}
+    for index_name in index_columns:
+        labels[index_name] = np.sort(df[index_name].unique())
+
+    # Specify GridSpec:
+    widths = np.array([unit] * len(labels) + [6*unit])
+    heights = np.array([unit] * len(df) + [legend_offset*unit])
+    gs_kw = dict(width_ratios=widths, height_ratios=heights)
+    fig, axs = plt.subplots(
+        ncols=len(widths), nrows=len(heights), 
+        constrained_layout=False, gridspec_kw=gs_kw, 
+        figsize=(scale * widths.sum(), scale * heights.sum()))
+
+    # Define color mappings:
+    from matplotlib.cm import get_cmap
+    colors = {}
+    cmaps = cycle(['Blues', 'Oranges', 'Greens', 'Reds', 'Purples', ])
+    for c, ((label, items), cmap_name) in enumerate(zip(labels.items(), cmaps)):
+        cmap = get_cmap(cmap_name)
+        inx_colors = {}
+        for value in items:
+            inx_colors[value] = cmap(0.3 + items.tolist().index(value) * 0.2)
+        colors[label] = inx_colors
+
+    for r, row in enumerate(axs):
+        for c, ax in enumerate(row):
+            ax.set(xticks=[], yticks=[])
+            ax.spines['top'].set_visible(False)
+            ax.spines['right'].set_visible(False)
+            ax.spines['bottom'].set_visible(False)
+            ax.spines['left'].set_visible(False)
+            if r == len(axs) - 1:
+                ax.patch.set_alpha(0.)
+                continue
+            if c < len(labels):
+                var_name = list(labels.keys())[c]
+                var_value = df[var_name][r]
+                ax.add_patch(pch.Rectangle((0., 0.), 1., 1., 
+                    facecolor=colors[var_name][var_value], edgecolor='black', label=var_value))
+                ax.spines['top'].set_linewidth(border)
+                ax.spines['right'].set_linewidth(border)
+                ax.spines['bottom'].set_linewidth(border)
+                ax.spines['left'].set_linewidth(border)
+            else:
+                cmap = get_cmap('Greys')
+                ax.patch.set_alpha(0.)
+                inter_ = 0.1
+                slope_ = (1. - inter_) / len(metric_columns)
+                for i, (metric_name, error) in enumerate(zip(metric_columns, error_columns)):
+                    clr = cmap(inter_ + (i + 1) * slope_)
+                    ax.barh([i], [df[metric_name][r]], xerr=[[0.], [df[error][r]]], 
+                        color=clr, edgecolor='black', height=1., capsize=capsize, label=metric_name)
+                handles, lbs = ax.get_legend_handles_labels()
+
+    # Build the legend:
+    sizes = [ll.shape[0] for ln, ll in labels.items()]
+    largest_label_inx = np.argmax(sizes)
+
+    ## Categorical variables:
+    hndl = []
+    for i, (lbl, colors_) in enumerate(colors.items()):
+        size, empty = len(colors_), []
+        if i >= 1:
+            m = sizes[largest_label_inx] - sizes[i - 1]
+            if m > 0:
+                empty = [Line2D([], [], label='', alpha=0.)] * m
+        hndl += empty + [Line2D([], [], label=lbl, alpha=0.)] + [
+            pch.Patch(facecolor=color, edgecolor="k", label=label, alpha=0.7) 
+            for label, color in colors_.items()
+        ]
+
+    ## Continuous variables:
+    empty = []
+    m = sizes[largest_label_inx] - sizes[-1] + 1
+    if m > 0:
+        empty = [Line2D([], [], label='', alpha=0.)] * m
+    hndl += empty + handles + [
+        Line2D([], [], label='', alpha=0.)] * (sizes[largest_label_inx] - len(metric_columns))
+        
+    legend = fig.legend(handles=hndl, loc='lower center', handlelength=scale*1.4, handleheight=scale*1.6, ncol=len(index_columns) + 1, labelspacing=.0)
+    legend.get_frame().set_alpha(0.)
+    plt.subplots_adjust(hspace=0.1, wspace=0.)
+    return fig
+
+def main(data_dir, data_filename, headers_filename):
+    """Run main script"""
+
+    # read in the headers
+    data_used = pd.read_csv(headers_filename)
+    #data_used = data[data["used_"]==True]
+    index_columns = list(data_used.loc[:, "indices"])
+    metrics_columns = list(data_used.loc[:, "metrics"])
+    error_columns = list(data_used.loc[:, "errors"])
+    headers = list(chain(index_columns, metrics_columns, error_columns))
+
+    # read in the data
+    # df = pd.read_csv(data_filename, usecols=headers )
+    df = pd.read_csv(io.StringIO(csv_str))
+
+    # make the plot
+    index_columns=['Index 1', 'Index 2', 'Index 3', 'Index 4']
+    metric_columns = ['Metric 1 Ave', 'Metric 2 Ave', 'Metric 3 Ave', 'Metric 4 Ave']
+    error_columns = ['Metric 1 Error', 'Metric 2 Error', 'Metric 3 Error', 'Metric 4 Error']
+    fig = dicrete_plot(df, index_columns, metric_columns, error_columns, scale=1.5, legend_offset=0.5, capsize=5)
+
+    # Export to svg file:
+    fig.savefig('test.svg')
+
+# Run main
+if __name__ == "__main__":
+    # Demo data
+    csv_str = """
+"Index 1","Index 2","Index 3","Index 4","Metric 1 Ave","Metric 1 Error","Metric 2 Ave","Metric 2 Error","Metric 3 Ave","Metric 3 Error","Metric 4 Ave","Metric 4 Error"
+A,B,B,D,10.2,2.1,14.4,1.3,12.4,2.3,10.4,2.3
+B,C,C,A,11.3,1.5,13.2,2.3,10.2,1.3,15.2,1.3
+A,A,A,C,10.2,2.4,12.1,4.1,10.1,1.1,14.4,1.4
+D,C,C,B,14.9,1.2,11.7,2.5,11.7,5.5,11.7,5.5
+E,C,C,B,14.9,1.2,10.7,2.5,10.7,0.5,10.7,0.5
+F,A,A,A,14.9,1.2,14.7,2.5,11.7,1.5,11.7,1.5
+"""
+    # Input files
+    data_dir = ""
+    data_filename = data_dir + "MultiIndicesBarPlotInput.csv"
+    headers_filename = data_dir + "MultiIndicesBarPlotHeaders.csv"
+
+    main(data_dir, data_filename, headers_filename)
